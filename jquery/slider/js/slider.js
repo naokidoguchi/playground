1ddd65ecd1e4692cc9e1020bee5ad784a4e9ef56
nao <naoaru.28@gmail.com>
2016-03-11 16:57:41 +0900

    スライダー１完成

--- jquery/slider/js/slider.js
+++ jquery/slider/js/slider.js
@@ -1,21 +1,27 @@
 $(function() {
 
-	var itemList = [
-		$('.js-slider__item:nth-child(1)'),
-		$('.js-slider__item:nth-child(2)'),
-		$('.js-slider__item:nth-child(3)')
-	]
+	var $slider = $('.slider'),
+	 		$item = $('.slider__item');
 
+			setInterval(slider, 2000);
 
+	function slider() {
 
-	var event = function() {
-		var i = 0;
-		i++
-		itemList[i].fadeOut();
-	};
+			wrap = $slider.width(),
+			size = -300, // ここに動かしたい画像分widthを入れる
+			int = parseInt(size),
+			sAbs = Math.abs(int),
+			num = $item.length,
+			left = $slider.css('left'),
+			mLeft = parseInt(left),
+			slideSize = Math.abs(mLeft);
 
-	for(var i=0; i<=2; i++) {
-		setInterval(event, 2000);
+			if(slideSize >= wrap - sAbs) {
+				$slider.animate({left: '0'}, 'slow');
+			} else {
+				$slider.animate({left: '+=' + int + 'px'}, 'slow');
+			}
+			console.log(slideSize,wrap - sAbs);
 	}
 
 });
