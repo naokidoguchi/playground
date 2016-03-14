bc5b50b855f74c53afd606f6c0f988d43b88709c
naokidoguchi <naoaru.28@gmail.com>
2016-03-14 11:43:52 +0900

    slider2完成

--- jquery/slider/js/slider.js
+++ jquery/slider/js/slider.js
@@ -14,14 +14,12 @@ $(function() {
 			mLeft = parseInt(left),
 			slideSize = Math.abs(mLeft);
 
-
-
 			if(slideSize >= wrap - sAbs) {
 				$slider.animate({left: '0'}, 'slow');
 			} else {
 				$slider.animate({left: '+=' + int + 'px'}, 'slow');
 			}
-			// console.log(slideSize,wrap - sAbs);
+			console.log(slideSize,wrap - sAbs);
 	}
 
 });
@@ -32,26 +30,39 @@ $(function() {
 	var $slider = $('.slider--2'),
 	$prev = $('span:first-child'),
 	$next = $('span:nth-child(2)'),
+	$item = $('.slider__item--2'),
 
 	wrap = $slider.width(),
-	size = -300, // ここに動かしたい画像分widthを入れる
+	size = -300, // ここに動かしたい画像分widthを入れるだけ。あとcssで全体のwidthを設定。
 	int = parseInt(size),
 	sAbs = Math.abs(int),
 	left,
 	mLeft,
-	slideSize = Math.abs(mLeft);
+	slideSize = Math.abs(mLeft),
+	item = $item.length,
+	clickCount = 1;
 
 	$prev.click(function() {
-		$slider.animate({left: '+=' + sAbs + 'px'}, 'slow');
+		if(clickCount <= 1) {
+			return false;
+		} else {
+			$slider.animate({left: '+=' + sAbs + 'px'}, 'slow');
+			clickCount--;
+			console.log(clickCount);
+		}
+
 	});
+
 	$next.click(function() {
 		left = $slider.css('left');
 		mLeft = Math.abs(left);
 
-		if(mLeft >= wrap - sAbs) {
-			$next.hide();
+		if(clickCount > item - 1) {
+			return false;
 		} else {
 			$slider.animate({left: '+=' + int + 'px'}, 'slow');
+			clickCount++;
+			console.log(clickCount);
 		}
 
 	});
