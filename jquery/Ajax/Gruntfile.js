f7350eff78a35776381f272f24fefd139962ab3c
naokidoguchi <naoaru.28@gmail.com>
2016-04-05 15:53:39 +0900

    knockout練習

---
+++ jquery/Ajax/Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    hostname: 'localhost'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: 'js/_src/*.js',
+                tasks: ['concat', 'uglify']
+            },
+            sass: {
+                files: '_sass/*.scss',
+                tasks: ['sass', 'cssmin']
+            },
+            haml: {
+                files: 'haml/*.haml',
+                tasks: ['haml']
+            }
+        },
+        concat: {
+            common: {
+                files: {
+                    'js/all.js': [
+                        'js/_src/test.js',
+                        'js/_src/test02.js'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            common: {
+                files: {
+                    'js/all.min.js': 'js/all.js'
+                }
+            }
+        },
+        sass: {
+            common: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/style.css': '_sass/style.scss'
+                }
+            }
+        },
+        cssmin: {
+            compress: {
+                files: {
+                    'css/style-min.css': 'css/style.css'
+                }
+            }
+        },
+        haml: {
+            dist: {
+                files: {
+                    'index.html': 'haml/index.haml'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('default', ['connect']);
+
+}
