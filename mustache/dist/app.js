2029ebc36055e8bccca799b49e7b63096c9075a3
nao <naoaru.28@gmail.com>
2016-04-26 16:38:00 +0900

    mustache update

--- mustache/dist/app.js
+++ mustache/dist/app.js
@@ -1,8 +1,19 @@
 $(function() {
-    var hoge = '{{name}}';
-    var html = Mustache.to_html(template, {
-          "response": {
-            "name": "くまお"
-        }
-    });
+  $.get('template1.html', function(template) {
+    var rendered = Mustache.render(template, {
+      msg1: "テンプレート１を読み込んでます"
+    }
+  );
+    $('#target1').html(rendered);
   });
+  $.get('template2.html', function(template) {
+    var rendered = Mustache.render(template, {
+      msg2: "テンプレート２を読み込んでます"
+    }
+  );
+    $('#target2').html('');
+    for(var i = 1; i < 10; i++) {
+      $('#target2').append('<li>' + rendered + '</li>');
+    }
+  });
+});
