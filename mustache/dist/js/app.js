ce031faca368b0ea9ae187d8ee4dd5bd185466e4
nao <naoaru.28@gmail.com>
2016-05-11 13:15:12 +0900

    update

---
+++ mustache/dist/js/app.js
@@ -0,0 +1,19 @@
+$(function() {
+  $.get('../template/template1.html', function(template) {
+    var rendered = Mustache.render(template, {
+      msg1: "テンプレート１を読み込んでます"
+    }
+  );
+    $('#target1').html(rendered);
+  });
+  $.get('../template/template2.html', function(template) {
+    var rendered = Mustache.render(template, {
+      msg2: "テンプレート２を読み込んでます"
+    }
+  );
+    $('#target2').html('');
+    for(var i = 1; i < 10; i++) {
+      $('#target2').append('<li>' + rendered + '</li>');
+    }
+  });
+});
