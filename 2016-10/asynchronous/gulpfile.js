54927d9083e04ee89d7dbba417172652a11140c1
Nao Kidoguchi <nao.kidoguchi@O-06470-MAC.local>
2016-10-25 12:48:53 +0900

    環境作成

---
+++ 2016-10/asynchronous/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function () {
+  gulp.src('dist')
+    .pipe(webserver({
+        host: 'localhost',
+        port: 8000,
+        livereload: true
+    }));
+});
+
+gulp.task('default', ['webserver'], function() {
+  console.log('きたか…!!\n　 ( ﾟдﾟ)　ｶﾞﾀｯ\n　 /　　 ヾ\n＿_L| /￣￣￣/＿\n　 ＼/　　　/');
+});
