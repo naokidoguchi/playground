9b6eb64b7b028c8ae4a3b3a763d1aea7fb5c7cfd
Nao Kidoguchi <nao.kidoguchi@O-06470-MAC.local>
2016-10-27 18:15:29 +0900

    timer css追加

--- countdown/js/countdown.js
+++ countdown/js/countdown.js
@@ -1,38 +1,39 @@
 ;(function (window){
     var int,
-        test,
+        timerArea,
         remainTime,
         timerId;
 
-function init() {
-    test = document.getElementById('js-timer');
-    remainTime = test.getAttribute('data-remaintime');
-    int = parseInt(remainTime);
-    test.innerHTML = computeDuration();
-    timerId = setInterval(setCountDown, 1000);     
-}
-
-function setCountDown() {
-    int = int - 1;
-    test = document.getElementById('js-timer');
-    test.innerHTML = computeDuration();
-    console.log(int > 0);
-    if(int <= 0) {
-        clearInterval(timerId);
+    function init() {
+        timerArea = document.getElementById('js-timer');
+        remainTime = timerArea.getAttribute('data-remaintime');
+        int = parseInt(remainTime);
+        timerArea.innerHTML = computeDuration();
+        timerId = setInterval(setCountDown, 1000);     
     }
-}
 
-function computeDuration() {
-    var  h, m, s;
-    h = '' + (int / 36000 | 0) + (int / 3600 % 10 | 0);
-    m = '' + (int % 3600 / 600 | 0) + (int % 3600 / 60 % 10 | 0);
-    s = '' + (int % 60 / 10 | 0) + (int % 60 % 10); 
-    return h + ':' + m + ':' + s;
-}
+    function setCountDown() {
+        int = int - 1;
+        timerArea = document.getElementById('js-timer');
+        timerArea.innerHTML = computeDuration();
+        console.log(int > 0);
+        if(int <= 0) {
+            clearInterval(timerId);
+            console.log('タイマー終わり');
+        }
+    }
 
-window.onload = function() {
-    init();
-};
+    function computeDuration() {
+        var  h, m, s;
+        h = '' + (int / 36000 | 0) + (int / 3600 % 10 | 0);
+        m = '' + (int % 3600 / 600 | 0) + (int % 3600 / 60 % 10 | 0);
+        s = '' + (int % 60 / 10 | 0) + (int % 60 % 10); 
+        return h + '時間' + m + '分' + s + '秒';
+    }
+
+    window.onload = function() {
+        init();
+    };
 
 })(window);
 
