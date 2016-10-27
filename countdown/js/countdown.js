dba1a8947bfaea28ad73d2394990c1c75b2a98c7
Nao Kidoguchi <nao.kidoguchi@O-06470-MAC.local>
2016-10-27 17:35:19 +0900

    timer完成？

--- countdown/js/countdown.js
+++ countdown/js/countdown.js
@@ -8,18 +8,26 @@ function init() {
     test = document.getElementById('js-timer');
     remainTime = test.getAttribute('data-remaintime');
     int = parseInt(remainTime);
-    test.innerHTML = int;
+    test.innerHTML = computeDuration();
     timerId = setInterval(setCountDown, 1000);     
 }
 
 function setCountDown() {
-    int = int - 1000;
+    int = int - 1;
     test = document.getElementById('js-timer');
-    test.innerHTML = int;
+    test.innerHTML = computeDuration();
     console.log(int > 0);
     if(int <= 0) {
         clearInterval(timerId);
     }
+}
+
+function computeDuration() {
+    var  h, m, s;
+    h = '' + (int / 36000 | 0) + (int / 3600 % 10 | 0);
+    m = '' + (int % 3600 / 600 | 0) + (int % 3600 / 60 % 10 | 0);
+    s = '' + (int % 60 / 10 | 0) + (int % 60 % 10); 
+    return h + ':' + m + ':' + s;
 }
 
 window.onload = function() {
