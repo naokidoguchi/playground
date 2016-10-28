d744052a0413d35d24178f112f2e33311c2cd6eb
Nao Kidoguchi <nao.kidoguchi@O-06470-MAC.local>
2016-10-28 17:18:54 +0900

    countdownレビュー済み

---
+++ countdown/js/stopwatch.js
@@ -0,0 +1,16 @@
+;(function() {
+    var id;
+    
+    function init() {
+        setStopwatch();
+    }
+
+    function setStopwatch() {
+        id = document.getElementById('js-stopwatch');
+        console.log(id);
+
+    }
+
+});
+
+//    <div id="js-stopwatch" data-countup="18000">target</div>
