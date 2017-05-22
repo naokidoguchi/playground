44db4e6ed821b0320d702e87d956cfd29cddb7f7
Nao Kidoguchi <naoaru.28@gmail.com>
2017-05-22 15:39:24 +0900

    add pug

---
+++ pug/gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const browserSync = require("browser-sync");
+const plumber = require('gulp-plumber');
+const pug = require('gulp-pug');
+const notifier = require('node-notifier');
+
+const errorHandler = function(error) {
+    notifier.notify({
+      message: 'しっぱいしたワン',
+      title: 'パグ',
+      appIcon: __dirname + '/pug.jpg',
+    }, function () {
+      console.log(error.message);
+    });
+  };
+
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./html/*.html", browserSync.reload);
+});
+
+gulp.task('views', function() {
+  return gulp.src(['./*.pug', '!./_*.pug'])
+    .pipe(plumber({errorHandler: errorHandler}))
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest('./html/'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(['./**/*.pug'], () => {
+    gulp.start(['views']);
+  });
+});
+
+gulp.task('default', ['browser-sync', 'views', 'watch']);
