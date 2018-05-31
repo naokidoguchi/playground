f26f35986e76ef9f26246e43bf8c23021ef25e2a
くまお <naokidoguchi@users.noreply.github.com>
2018-05-31 15:37:53 +0900

    create confirmation.php

---
+++ form_sample/confirmation.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>確認</title>
+</head>
+
+<body>
+
+    <h2>問合せ内容</h2>
+
+    <form action="mailto.php" method="post">
+
+        <table border="1">
+            <tr>
+                <td>名前</td>
+                <td>
+                    <?php echo $_POST["name"]; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>メールアドレス</td>
+                <td>
+                    <?php echo $_POST["mail"]; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>問い合わせ内容</td>
+                <td>
+                    <?php echo $_POST["inquiry"]; ?>
+                </td>
+            </tr>
+        </table>
+
+        <input type="submit" value="送信" />
+    </form>
+
+</body>
+
+</html>
