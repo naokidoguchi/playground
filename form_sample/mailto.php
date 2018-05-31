1354e0b047c19d2285f0db6869d1d289cb4be6ae
くまお <naokidoguchi@users.noreply.github.com>
2018-05-31 15:38:22 +0900

    create mailto.php

---
+++ form_sample/mailto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $lang = 'ja';
+        // 三項演算子
+        echo $lang == 'ja' ? '<script>alert("ありがとうございます！");</script>' : '<script>alert("THANX!");</script>';
+    ?> 
+</body>
+</html>
+
